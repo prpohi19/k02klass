861dcc1a568967a25a9971b18d2c7cd18f72156e
kertliinat <43129546+kertliinat@users.noreply.github.com>
2019-02-20 11:22:20 +0200

    Add files via upload

---
+++ Teleporter.java
@@ -0,0 +1,14 @@
+class Teleporter{
+
+  Tyyp tyyp;
+  int x1,y1;
+
+//lisasin x ja y väärtustele ühed, et vältida konflikti tegelase klassi x-de ja y-tega
+  public Teleporter(){
+		tyyp=Tyyp.TELEPORTER;
+}
+
+
+
+
+}
