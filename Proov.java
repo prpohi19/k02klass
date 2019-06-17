2dd463fe9f901aed4290508420b5bb3442e8258d
Revor Jan Nuiamäe <43164694+rnuiame@users.noreply.github.com>
2019-06-17 17:38:27 +0300

    Create Proov.java

---
+++ Proov.java
@@ -0,0 +1,11 @@
+public class Proov{
+    int summa, lahutus;
+    public int liida(int esimene, int teine){
+        summa = esimene + teine;
+        return summa;
+    }
+    public int lahuta(int esimene, int teine){
+        lahutus = esimene - teine;
+        return lahutus;
+    }
+}
